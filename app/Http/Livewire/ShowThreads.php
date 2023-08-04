b6e9698df27e58e4c7e55e03a91feb14fc2e2456
SammyJoseph <sam.tab.paz@gmail.com>
2023-08-04 15:21:01 -0500

    clase #6 finalizada

--- app/Http/Livewire/ShowThreads.php
+++ app/Http/Livewire/ShowThreads.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
 use Livewire\Component;
 
 class ShowThreads extends Component
 {
     public function render()
-    {
-        return view('livewire.show-threads');
+    {   
+        $categories = Category::all();
+
+        return view('livewire.show-threads', compact('categories'));
     }
 }
--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -3,16 +3,19 @@
         <a href="#" class="block w-full py-4 mb-10 bg-gradient-to-r from-blue-600 to-blue-700 hover:to-blue-600 text-white/90 font-bold text-xs text-center rounded-md">Preguntar</a>
 
         <ul>
+            @foreach ($categories as $category)
+                <li class="mb-2">
+                    <a href="#" class="p-2 rounded-md flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
+                        <span class="w-2 h-2 rounded-full" style="background-color: {{ $category->color }}"></span>
+                        {{ $category->name }}
+                    </a>
+                </li>
+            @endforeach
+            
             <li class="mb-2">
                 <a href="#" class="p-2 rounded-md flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
-                    <span class="w-2 h-2 rounded-full bg-green-400"></span>
-                    Ejemplo
-                </a>
-            </li>
-            <li>
-                <a href="#" class="p-2 rounded-md flex bg-slate-800 items-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
-                    <span class="w-2 h-2 rounded-full bg-sky-400"></span>
-                    Ejemplo 2
+                    <span class="w-2 h-2 rounded-full bg-white"></span>
+                    Todas las categorías
                 </a>
             </li>
         </ul>
