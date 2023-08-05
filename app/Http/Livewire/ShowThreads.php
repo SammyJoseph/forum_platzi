09e7e5e9cadaa0bbb8f44432437f81e1403098ca
SammyJoseph <sam.tab.paz@gmail.com>
2023-08-04 19:35:50 -0500

    clase #11 finalizada

--- app/Http/Livewire/ShowThreads.php
+++ app/Http/Livewire/ShowThreads.php
@@ -7,12 +7,24 @@ use App\Models\Thread;
 use Livewire\Component;
 
 class ShowThreads extends Component
-{
+{   
+    public $search = "";
+
     public function render()
     {   
         $categories = Category::all();
-        $threads = Thread::latest()->withCount('replies')->get();
 
-        return view('livewire.show-threads', compact('categories', 'threads'));
+        /* Mi método */
+        // $threads = Thread::where('title', 'LIKE', "%{$this->search}%")->latest()->withCount('replies')->get();
+
+        /* Método del profesor (no se puede enviar con compact sino como propiedad) */
+        $threads = Thread::query();
+        $threads->where('title', 'LIKE', "%{$this->search}%");
+        $threads->withCount('replies');
+        $threads->latest();
+
+        return view('livewire.show-threads', compact('categories'), [
+            'threads' => $threads->get() // se envía como propiedad
+        ]);
     }
 }
