98549f8f69882e06f0645812abfc3f952d39611c
Sammy Joseph <sam.tab.paz@gmail.com>
2023-08-05 21:38:32 -0500

    clase #23 finalizada

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -7,7 +7,28 @@ use App\Models\Category;
 use App\Models\Thread;
 
 class ThreadController extends Controller
-{
+{   
+    public function create(Thread $thread)
+    {
+        $categories = Category::all();
+
+        return view('thread.create', compact('categories', 'thread'));
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'title' => 'required|min:5',
+            'body' => 'required|min:5',
+            'category_id' => 'required|exists:categories,id'
+        ]);
+
+        /* threads() es una relacion de user con thread */
+        $thread = auth()->user()->threads()->create($request->all()); // crea la pregunta con el usuario autenticado
+
+        // return redirect()->route('thread', $thread); // redirecciona a la ruta thread (show de livewire) con la pregunta creada
+        return redirect()->route('dashboard');
+    }
+
     public function edit(Thread $thread) // recibe el modelo Thread y lo busca por id
     {
         $categories = Category::all();
--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -2,7 +2,7 @@
 
     {{-- Columna 1 - Lista de categorías --}}
     <div class="w-64">
-        <a href="#" class="block w-full py-4 mb-6 bg-gradient-to-r from-blue-600 to-blue-700 hover:to-blue-600 text-white/90 font-bold text-sm text-center rounded-md uppercase">Preguntar</a>
+        <a href="{{ route('threads.create') }}" class="block w-full py-4 mb-6 bg-gradient-to-r from-blue-600 to-blue-700 hover:to-blue-600 text-white/90 font-bold text-sm text-center rounded-md uppercase">Preguntar</a>
 
         <ul>
             @foreach ($categories as $category)
@@ -60,10 +60,12 @@
                                     <span class="text-sky-400 ml-2">{{ $thread->created_at->diffForHumans() }}</span>
                                 </span>
                                 <span class="flex items-center gap-1 text-slate-700">
-                                    <svg class="h-4" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"></path>
-                                    </svg>
-                                    {{ $thread->replies_count }} {{ $thread->replies_count !== 1 ? 'respuestas' : 'respuesta' }}
+                                    <a href="{{ route('thread', $thread) }}" class="hover:text-white flex items-center gap-1">
+                                        <svg class="h-4" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"></path>
+                                        </svg>
+                                        {{ $thread->replies_count }} {{ $thread->replies_count !== 1 ? 'respuestas' : 'respuesta' }}
+                                    </a>
                                     
                                     |
                                     
